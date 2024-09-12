e0b11cd49380548f335055e97c2e28a3f4438c23
Gaby <gabytorna@gmail.com>
2024-09-12 11:57:41 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+var questionsArr = [
+        {
+            question: 'Is Harry Styles debut album called Harry Styles?',
+            answer: true
+        },
+        {
+            question:'Harry was the lead singer of the band, The Wanted.',
+            answer: false
+        },
+        {
+            question:'There were 5 members in 1D.',
+            answer: true   
+        },    
+        {
+            question:'Love on Tour and Live on Tour are names of Harrys tours.',
+            answer: true
+        },
+        {
+            question:'Harry is American.',
+            answer: false
+        }
+
+    ];
+    function runQuiz() {
+        var score=0;
+        for(var i = 0; i < questionsArr.length; i++) {
+            var userAnswer = confirm(questionsArr[i].question)
+       
+        if (userAnswer === questionsArr[i].answer) {
+            score++; 
+        }
+    }
+    var finalScore = Math.round((score / questionsArr.length) * 100);
+    alert((`Your final score is ${finalScore}%`));
+}
+
+    
